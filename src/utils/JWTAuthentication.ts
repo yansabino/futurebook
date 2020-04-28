aac68e01228d5d169c14254f4f6e5f2b25a52823
Yan Sabino <yan.sabino@hotmail.com>
2020-04-28 14:17:24 -0300

    some protection done

--- src/utils/JWTAuthentication.ts
+++ src/utils/JWTAuthentication.ts
@@ -3,9 +3,11 @@ import {
   AuthenticationGateway,
   UsersInfoForToken
 } from "../business/gateways/authenticationGateway";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 export class JWTAuthentication implements AuthenticationGateway {
-  private SECRET_KEY = "bananinha";
+  private SECRET_KEY = process.env.SECRET_KEY as string;
   private expiresIn = "10h";
 
   public generateToken(input: UsersInfoForToken): string {
